182dd8c369c2b7a24c1661506ad17041bbfb1301
whoproami <iemagn78@gmail.com>
2024-04-29 19:59:28 +0530

    HI ther

--- src/Screens/App.js
+++ src/Screens/App.js
@@ -20,15 +20,21 @@ function App() {
 
             </header>
             <main>
+                <div className="cards">
                 <div>
+                  <h3>Study Materials</h3>  
                 </div>
                 <div>
+                 <h3>Vaccines and Examdates</h3> 
                 </div>
                 <div>
-                  
+                  <h3>Educator</h3>
                 </div>
-                <img src="../assets/girl.png" alt="Girl Studying With Phone"></img>
-                <p></p>
+                </div>
+                <img src={require("../assets/girl.png")} alt="Girl Studying With Phone"/>
+                <p>
+                Welcome to HamroSikshya.online, your premier destination for affordable educational resources in Nepal! We're dedicated to making learning accessible to all, offering a wide range of low-cost notes tailored to school, college, and government education curricula. Whether you're a student striving for academic excellence or an educator seeking supplemental materials, our platform provides the leverage you need to excel. Explore our comprehensive collection and unlock the power of online learning with HamroSikshya.online.
+                </p>
             </main>
             <footer>
 
