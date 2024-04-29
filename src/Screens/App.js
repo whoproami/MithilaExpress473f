95397cf89941e13c027370b78207155476ae0734
whoproami <iemagn78@gmail.com>
2024-04-29 19:31:19 +0530

    New

---
+++ src/Screens/App.js
@@ -0,0 +1,39 @@
+import "./App.css"
+function App() {
+    return (
+        <div id="body">
+            <header>
+                <span id="Logo">
+                hamrosikshya.online
+                </span>
+                <div id="FooterRight">
+                <div>
+                  Study Materials
+                </div>
+                <div id="dropbar">
+                  Vaccines and Examdates
+                </div>
+                <div>
+                  About us
+                </div>
+                </div>
+
+            </header>
+            <main>
+                <div>
+                </div>
+                <div>
+                </div>
+                <div>
+                  
+                </div>
+                <img src="../assets/girl.png" alt="Girl Studying With Phone"></img>
+                <p></p>
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
+export default App;
